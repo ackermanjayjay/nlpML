659dbaccb4ca1dd416c16e0f6c8f0604670d8994
ackermanjayjay <reza23198@gmail.com>
2024-03-17 13:39:45 +0700

    fix: source path fixed

--- src/loadedModel.py
+++ src/loadedModel.py
@@ -3,13 +3,14 @@ import pickle
 import os
 
 
-def Prediction(text: str):
+def Prediction(text):
     loaded_vec = TfidfVectorizer(
         decode_error="replace",
-        vocabulary=set(pickle.load(open(os.path.join("tfidf\idf.pkl"), "rb"))),
+        vocabulary=set(pickle.load(open(os.path.join("tfidf", "idf.pkl"), "rb"))),
     )
 
-    predictor_load_dec_pakaian = pickle.load(open("model\knn_model.pkl", "rb"))
+    predictor_load = pickle.load(open(os.path.join("model", "knn_model.pkl"), "rb"))
 
-    tfidf_pakaian = loaded_ve.fit_transform(text)
-    return predictor_load_dec_pakaian.predict(tfidf_pakaian)
+    tfidf_newData = loaded_vec.fit_transform([text])  # Transform the input text
+    result = predictor_load.predict(tfidf_newData)
+    return result[0]
